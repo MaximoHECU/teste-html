ae92c7f16029cf0629ff44d3f6c1a98fadb0aa7e
Pedro Maximo <aluno10pe.ph@gmail.com>
2026-04-22 20:31:06 -0300

    Fiz o exercício 1.2

--- exercicios.js
+++ exercicios.js
@@ -3,3 +3,16 @@ function inverterString(frase){
 
     console.log("O inverso da frase é " + inverso);
 }
+
+function contarVogais(frase){
+    const fraseMin = frase.toLowerCase("").split("");
+    var qtd = 0;
+
+    for(i = 0; i < fraseMin.length; i++){
+        if (fraseMin[i] == "a" || fraseMin[i] == "e" || fraseMin[i] == "i" || fraseMin[i] == "o" || fraseMin[i] == "u"){
+            qtd += 1; 
+        }
+    }
+
+    console.log("Quantidade de vogais: " + qtd);
+}
